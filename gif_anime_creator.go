3111a2173970c2490a4db37eb47795494e748d32
midnight.walker <from.midnight.walker@gmail.com>
2017-12-30 09:50:08 +0900

    Added:
      - loop flag (if true, picture starts from center otherwise starts from
        outside)

--- gif_anime_creator.go
+++ gif_anime_creator.go
@@ -22,10 +22,13 @@ func main() {
 	// command line arguments
 	var argStartFlag string
 	var argEndFlag string
+	var argLoopFlag string
 	// starting position
 	flag.StringVar(&argStartFlag, "start", "left", "Please set direction for gif animation.")
 	// ending position
 	flag.StringVar(&argEndFlag, "end", "right", "Please set direction for gif animation.")
+	// loop flag
+	flag.StringVar(&argLoopFlag, "loop", "false", "Please set direction for gif animation.")
 	flag.Parse()
 
 	const postFix string = "_animated"
@@ -76,36 +79,62 @@ func main() {
 	var delays []int
 	var disposal []byte
 	rect := decodedImage.Bounds()
-	log.Println(argStartFlag)
-	log.Println(argEndFlag)
-	for i := 1; i <= 9; i++ {
+	for i := 0; i < 10; i++ {
 		var drawXBound int
 		var drawYBound int
 		drawXBound = 0
 		drawYBound = 0
-		if i <= 5 { // for first 5 images
-			switch argStartFlag {
-			case "right":
-				drawXBound = -rect.Dx() + int(math.Floor(float64(rect.Dx())*0.2*float64(i)))
-			case "left":
-				drawXBound = rect.Dx() - int(math.Floor(float64(rect.Dx())*0.2*float64(i)))
-			case "top":
-				drawYBound = rect.Dy() - int(math.Floor(float64(rect.Dy())*0.2*float64(i)))
-			case "bottom":
-				drawYBound = -rect.Dy() + int(math.Floor(float64(rect.Dy())*0.2*float64(i)))
-			default:
+		if i < 5 { // for first 5 images
+			if argLoopFlag == "true" { // if true, start from center
+				switch argStartFlag {
+				case "right":
+					drawXBound = -rect.Dx() + int(math.Floor(float64(rect.Dx())*0.2*float64(i+5)))
+				case "left":
+					drawXBound = rect.Dx() - int(math.Floor(float64(rect.Dx())*0.2*float64(i+5)))
+				case "top":
+					drawYBound = rect.Dy() - int(math.Floor(float64(rect.Dy())*0.2*float64(i+5)))
+				case "bottom":
+					drawYBound = -rect.Dy() + int(math.Floor(float64(rect.Dy())*0.2*float64(i+5)))
+				default:
+				}
+			} else {
+				switch argStartFlag {
+				case "right":
+					drawXBound = -rect.Dx() + int(math.Floor(float64(rect.Dx())*0.2*float64(i)))
+				case "left":
+					drawXBound = rect.Dx() - int(math.Floor(float64(rect.Dx())*0.2*float64(i)))
+				case "top":
+					drawYBound = rect.Dy() - int(math.Floor(float64(rect.Dy())*0.2*float64(i)))
+				case "bottom":
+					drawYBound = -rect.Dy() + int(math.Floor(float64(rect.Dy())*0.2*float64(i)))
+				default:
+				}
 			}
 		} else { // for last 5 images
-			switch argEndFlag {
-			case "right":
-				drawXBound = -int(math.Floor(float64(rect.Dx()) * 0.2 * float64(i-5)))
-			case "left":
-				drawXBound = int(math.Floor(float64(rect.Dx()) * 0.2 * float64(i-5)))
-			case "top":
-				drawYBound = int(math.Floor(float64(rect.Dy()) * 0.2 * float64(i-5)))
-			case "bottom":
-				drawYBound = -int(math.Floor(float64(rect.Dy()) * 0.2 * float64(i-5)))
-			default:
+			if argLoopFlag == "true" {
+				switch argEndFlag {
+				case "right":
+					drawXBound = -int(math.Floor(float64(rect.Dx()) * 0.2 * float64(i-10)))
+				case "left":
+					drawXBound = int(math.Floor(float64(rect.Dx()) * 0.2 * float64(i-10)))
+				case "top":
+					drawYBound = int(math.Floor(float64(rect.Dy()) * 0.2 * float64(i-10)))
+				case "bottom":
+					drawYBound = -int(math.Floor(float64(rect.Dy()) * 0.2 * float64(i-10)))
+				default:
+				}
+			} else {
+				switch argEndFlag {
+				case "right":
+					drawXBound = -int(math.Floor(float64(rect.Dx()) * 0.2 * float64(i-5)))
+				case "left":
+					drawXBound = int(math.Floor(float64(rect.Dx()) * 0.2 * float64(i-5)))
+				case "top":
+					drawYBound = int(math.Floor(float64(rect.Dy()) * 0.2 * float64(i-5)))
+				case "bottom":
+					drawYBound = -int(math.Floor(float64(rect.Dy()) * 0.2 * float64(i-5)))
+				default:
+				}
 			}
 		}
 		drawPoint := image.Pt(drawXBound, drawYBound)
